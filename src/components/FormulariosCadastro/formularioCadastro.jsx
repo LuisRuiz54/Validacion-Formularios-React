49ed29ba389bf6cf2d77f01096e6c5408f33b88f
unknown <nessiuscaffe@gmail.com>
2021-07-15 18:50:00 -0300

    Aplicando Hooks e usando useState

--- src/components/FormulariosCadastro/formularioCadastro.jsx
+++ src/components/FormulariosCadastro/formularioCadastro.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import {TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+     const [nome,setNome] = useState("");
+     const [sobrenome,setSobrenome] = useState("");
+
+
   return (
-    <form>
-        <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
-        <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
+    <form 
+    onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome, sobrenome);
+    }}
+    >
+        <TextField 
+        value={nome}
+        onChange={(event) => {
+            let tmpNome = event.target.value;
+            if (tmpNome.length >= 3) {
+             tmpNome = tmpNome.substr(0, 3);
+            }
+            setNome(tmpNome);
+        }}
+
+        id="nome" 
+        label="Nome" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth/>
+        
+        <TextField 
+        value={sobrenome}
+        onChange={(event) => {
+            setSobrenome(event.target.value);
+        }}
+        id="sobrenome" 
+        label="Sobrenome" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth/>
+
+
         <TextField id="cpf" label="Cpf" variant="outlined" margin="normal" fullWidth/>
 
      <FormControlLabel control={<Switch 
